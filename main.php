85d8d123d64e2ce926aa87599af5ec7944b0e49c
xcqvmywoj <10210215@mail.chihlee.edu.tw>
2018-07-23 01:47:51 +0800

    Update

--- main.php
+++ main.php
@@ -1,3 +1,19 @@
 <?php
-  echo "123213";
+
+  function readnote($filepath){
+    if(is_file($filepath)){
+      $content = "";
+      foreach (file($filepath) as $value) {
+        $content .= $value;
+      }
+      return $content;
+    } else {
+      fopen($filepath,'w');
+      echo '已創建'.$filepath.'檔案，檔案大小：'.filesize($filepath).'byte';
+    }
+  }
+  
+  $content = readnote('./note.json');
+  echo "<div>{$content}</div>";
+
 ?>
